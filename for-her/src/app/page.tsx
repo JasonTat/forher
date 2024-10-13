b0f06f418c27d4cba76234f5b1f1ad639e7cc0fe
JasonTat <112578835+JasonTat@users.noreply.github.com>
2024-10-13 15:54:54 -0600

    update

--- for-her/src/app/page.tsx
+++ for-her/src/app/page.tsx
@@ -1,55 +1,124 @@
-import Image from "next/image";
+'use client';
+import { useState, useEffect } from 'react';
+import Image from 'react';
+
+//styles
 import styles from "../../styles/page.module.css";
 import homePageStyles from "../../styles/home-page.module.css";
 
 //custom components
 import Navigation from "../../components/Navigation";
 
+import CryingBear from '../../public/img/reactions/43ffe16a0deb6681d0e4074dfda2dc0b.gif'
+
 //MUI components
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid2';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 export default function Home() {
+
+
+  const [answer, setAnswer] = useState('');
+
+  function handleYesClick() {
+    setAnswer('yes');
+  }
+
+  function handleNoLoveClick() {
+    setAnswer('no');
+  }
+
+  function handleResetClick(){
+      setAnswer('reset');
+  }
+
+
   return (
 
     <>
       <Navigation />
-      <section>
-        <div className={homePageStyles.container}>
-          <div>
-            <h2>Hi Karen!</h2>
-          </div>
-          <div>
-            <p>
-              Click on some of these, I built them for you :)
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className={homePageStyles.funButtonsGrid}>
-        <Stack>
-
-          <div>
-            <div>
-              <Button>Click here if you love me</Button>
-            </div>
-          </div>
-
-
-
-        </Stack>
 
 
 
 
 
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <Box component="section" className={homePageStyles.container}>
 
-      </section>
+          <Grid container spacing={2} >
+            <Grid display="flex" justifyContent="center" alignItems="center" size={12}>
+              <h2>Hi Karen!</h2>
+            </Grid>
+            <Grid display="flex" justifyContent="center" alignItems="center" size={12}>
+              <Typography variant="body2">
+                Click on some of these, I built them for you :)
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+        
+        <Box component='section' className={homePageStyles.container2}>
+          <Stack spacing={1}
+            sx={{justifyContent: 'center', alignItems: 'center'}}
+            >
+            
+            <Box component='div'>
+            Do you love me?
+            </Box>
+
+            <Box className={homePageStyles.twoButtons}>
+
+              <Box component='div'>
+                <Button
+                 sx={{
+                  color: 'white',
+                  fontWeight: '700',
+                  backgroundColor: '#e77ed0',
+                  marginRight: '2px', 
+                  '&:hover': {
+                    backgroundColor: '#914186'
+                  }
+                }} 
+                className={homePageStyles.yesButton}
+                 onClick={() => handleYesClick()}>
+                  Yes
+                </Button>
+              </Box>
+
+              <Box component='div'>
+                <Button
+                 sx={{
+                  color: 'black',
+                  fontWeight: '700',
+                  backgroundColor: 'red',
+                  marginLeft: '2px',
+                  '&:hover': {
+                    backgroundColor: 'darkred'
+                  }              
+                }}
+                 onClick={() => handleNoLoveClick()}>
+                  No
+                </Button>
+              </Box>
+            </Box>
+
+
+            <Box component='div'>
+              <img src='https://i.pinimg.com/originals/43/ff/e1/43ffe16a0deb6681d0e4074dfda2dc0b.gif' className={homePageStyles.sadBear}/>
+            </Box>
+          </Stack>          
+        </Box>
+
+        <Box component='section'>
+         <Box component='div'>
+          <Typography variant='body2'>
+            {answer}
+          </Typography>
+         </Box>
+        </Box>
 
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
 For her
+
+- Made in React with TypeScript, simple front-end app. No backend because there is no need to keep anything on the backend for now.
+- Probably will have backend for my API to call "reasons why I love you" but that's it.
+- 
+
+- Kuromi because she loves Kuromi
+- Kuromi colour palette (Black, pink)
+
+
+Assets that aren't mine: 
+Crying Sage GIF - https://tenor.com/view/sage-crying-gif-26712775
+``` - <div class="tenor-gif-embed" data-postid="26712775" data-share-method="host" data-aspect-ratio="0.915625" data-width="100%"><a href="https://tenor.com/view/sage-crying-gif-26712775">Sage Crying Meme</a>from <a href="https://tenor.com/search/sage+crying-memes">Sage Crying Memes</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>```
+
+
